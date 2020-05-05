53237d91b811d09e4adaf50da0a903b7b5af372e
Stefan Clarke <41588897+stefanlclarke@users.noreply.github.com>
2020-05-05 22:58:13 +0100

    debugged stalemate and victory errors (hopefully)

--- play.py
+++ play.py
@@ -1,21 +1,22 @@
 import game
 from game import board
+import numpy as np
 
 board_size=6
 game = board(board_size)
 
 def play(loc1, loc2, move):
     victor, stalemate = game.makemove(np.array([loc1, loc2]), move)
-    #print(f"Victor: {victor}")
-    #print(f"Stalemate? {stalemate}")
+    print(f"Victor: {victor}")
+    print(f"Stalemate? {stalemate}")
     print("Game Board:")
     print(game.board)
     if victor != 0:
         print("GAME OVER!")
-        game.reset()
-    if stalemate:
+        #game.reset()
+    elif stalemate:
         print("STALEMATE")
-        game.reset()
+        #game.reset()
     return victor
 
 print(game.board)
