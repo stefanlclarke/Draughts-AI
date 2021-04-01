e2bf25d8031f5e06f5bff5bbce1926dcf9491369
stefanlclarke <stefan.slc.clarke@gmail.com>
2021-04-01 11:47:49 +0100

    added move memory

--- memory/memory.py
+++ memory/memory.py
@@ -9,17 +9,19 @@ class GameMemory:
         Class for pickling game-states to memory
         """
         self.memory = []
+        self.move_memory = []
         self.env = game_env
 
         self.memory.append(deepcopy(self.env.get_state()))
 
     def save_game(self):
         save_name = "memory/saved_games/{}.pickle".format(str(datetime.now())).replace(" ","")
-        pickle.dump(self.memory, open( save_name.replace(":",""), "wb" ))
+        pickle.dump([self.memory, self.move_memory], open( save_name.replace(":",""), "wb" ))
 
     def step(self, move):
         new_board, reward, done, illegal = self.env.step(move)
         self.memory.append(deepcopy(self.env.get_state()))
+        self.move_memory.append(move)
         if done:
             print('GAME OVER')
             self.save_game()
